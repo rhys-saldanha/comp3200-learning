a3ecc18dbcc4e90962d7260fe674b6f8b5c3d8c8
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-02-21 14:35:04 +0000

    Tidy code, small improvements

--- example.py
+++ example.py
@@ -9,7 +9,7 @@ y = tf.placeholder(dtype=tf.float64)
 x_train = [1, 2, 3, 4]
 y_train = [0, 1, 2, 3]
 
-linear_model = W * x + b
+linear_model = tf.multiply(W, x) + b
 
 squared_deltas = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_deltas)
@@ -21,18 +21,17 @@ init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
+    past = (0, 0)
+    curr = sess.run([W, b])
 
     error = 1
     target = 1e-20
     n = 0
 
-    while error > target:
-        _, error = sess.run([train, loss], feed_dict={x: x_train, y: y_train})
-        n += 1
-        if n == 1000:
-            curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
-            print("W : ", curr_W, ", b : ", curr_b, ", loss : ", curr_loss)
-            n = 0
+    while past != curr:  # Loop till variables are unchanged
+        past = curr
+        sess.run(train, feed_dict={x: x_train, y: y_train})
+        curr = sess.run([W, b], {x: x_train, y: y_train})
 
     curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
     print("W : ", curr_W, ", b : ", curr_b, ", loss : ", curr_loss)
