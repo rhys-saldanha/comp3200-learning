e4e514f940a7b51ad1e1882b3997aa51198d6e4c
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-02-22 15:54:56 +0000

    Lots of changes
    * comments show some of the best results

--- learn_exponential.py
+++ learn_exponential.py
@@ -1,54 +1,89 @@
+import argparse
+import math
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 import tensorflow as tf
 
-# Just wild type
+parser = argparse.ArgumentParser()
 
-alpha = tf.Variable([29931.0184531], dtype=tf.float64)
-a = tf.Variable([179722.16846192], dtype=tf.float64)
-b = tf.Variable([59989.11770581], dtype=tf.float64)
+parser.add_argument('--alpha', type=float, default=889)
+parser.add_argument('--a', type=float, default=4164)
+parser.add_argument('--b', type=float, default=667)
 
-c = tf.placeholder(dtype=tf.float64)
-X = tf.placeholder(dtype=tf.float64)
+args = parser.parse_args()
 
-T = tf.constant(1800, dtype=tf.float64)
+with tf.device('/cpu:0'):
+    # with tf.device('/device:gpu:0'):
 
-with open('data_wt_row1.csv', 'r') as f:
-    c_train = f.readline().strip().split(',')
-    X_train = f.readline().strip().split(',')
+    # all data
+    # alpha: 72502.10095009,    a: 339540.3056056,      b: 54449.33459445,      loss: 619784.3227130468
+    # alpha: 889.2980868261652, a: 4164.7414410585925,  b: 667.8660128354211,   loss: 619784.3227130426
+    # alpha: 3579.692612228826, a: 16764.340195841032,  b: 2688.361829989068,   loss: 619784.3227130463
+
+    # alpha: 2955.356560847476, a: 13840.462801104484, b: 2219.4832443860337, loss: 44814.461528813496
+    # alpha: 3020.3095022586476, a: 14144.649042933394, b: 2268.263133434123, loss: 44814.46152881349
+
+    alpha = tf.Variable(args.alpha, dtype=tf.float64)
+    a = tf.Variable(args.a, dtype=tf.float64)
+    b = tf.Variable(args.b, dtype=tf.float64)
+
+    c = tf.placeholder(dtype=tf.float64)
+    X = tf.placeholder(dtype=tf.float64)
+
+    T = tf.constant(1800, dtype=tf.float64)
+
+    death = tf.add(a, tf.multiply(b, c))
+    model = tf.multiply(tf.div(alpha, death), tf.add(T, tf.div(tf.exp(-tf.multiply(death, T)) - 1.0, death)))
+
+    squared_deltas = tf.square(model - X)
+    loss = tf.reduce_sum(squared_deltas)
+
+    optimiser = tf.train.MomentumOptimizer(1, .5, use_nesterov=True)
+    train = optimiser.minimize(loss=loss)
+
+    init = tf.global_variables_initializer()
+
+# with open('data_wt.csv', 'r') as f:
+#     c_train = f.readline().strip().split(',')
+#     X_train = f.readline().strip().split(',')
+
+c_train = []
+X_train = []
+# with open('data_wt.csv', 'r') as f:
+with open('data_wt_avg.csv', 'r') as f:
+    c_values = f.readline().strip().split(',')
+    for line in f:
+        c_train += c_values
+        X_train += line.strip().split(',')
 
 c_train = list(map(lambda x: float(x), c_train))
 X_train = list(map(lambda x: float(x), X_train))
 
-death = tf.add(a, tf.multiply(b, c))
-model = tf.multiply(tf.div(alpha, death), tf.add(T, tf.div(tf.exp(-tf.multiply(death, T)) - 1.0, death)))
-
-squared_deltas = tf.square(model - X)
-loss = tf.reduce_sum(squared_deltas)
-
-optimiser = tf.train.MomentumOptimizer(.001, 0.0001)
-train = optimiser.minimize(loss=loss)
-
-init = tf.global_variables_initializer()
+# print(c_train, '\n', X_train)
+# print(len(c_train), '\n', len(X_train))
 
 with tf.Session() as sess:
     sess.run(init)
-    past = (0, 0, 0)
-    # Evaluate values of parameters to learn
-    curr = sess.run([alpha, a, b])
 
-    error = 1
-    target = 1e-20
-    n = 0
+    curr_error = 1
+    past_error = 0
+    n = -1
 
-    while past != curr and error > target:
-        # Loop till variables are unchanged or loss is small enough
-        past = curr
-        sess.run(train, feed_dict={c: c_train, X: X_train})
-        curr = sess.run([alpha, a, b], {c: c_train, X: X_train})
-        error = sess.run(loss, {c: c_train, X: X_train})
-        n += 1
-        if n % 10**4 == 0:
-            n = 0
-            print('alpha:{}, a:{}, b:{}, loss:{}'.format(curr[0], curr[1], curr[2], error))
-
-    curr_alpha, curr_a, curr_b, curr_loss = sess.run([alpha, a, b, loss], {c: c_train, X: X_train})
-    print('alpha:{}, a:{}, b:{}, loss:{}'.format(curr_alpha, curr_a, curr_b, curr_loss))
+    try:
+        # while not math.isclose(past_error, curr_error):
+        while True:
+            past_error = curr_error
+            sess.run(train, {c: c_train, X: X_train})
+            curr_error = sess.run(loss, {c: c_train, X: X_train})
+            n += 1
+            if n % (10 ** 4) == 0:
+                n = 0
+                curr = sess.run([alpha, a, b, loss], {c: c_train, X: X_train})
+                print('alpha:{}, a:{}, b:{}, loss:{}'.format(curr[0], curr[1], curr[2], curr[3]))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        curr_alpha, curr_a, curr_b, curr_loss = sess.run([alpha, a, b, loss], {c: c_train, X: X_train})
+        print('alpha:{}, a:{}, b:{}, loss:{}'.format(curr_alpha, curr_a, curr_b, curr_loss))
